6d566fd3a231974c1e06581e02fd7e7df011056d
Rashad cureton <rashad.cureton@gmail.com>
2017-03-22 15:45:46 -0400

    updated build script

--- scripts/build.sh
+++ scripts/build.sh
@@ -3,13 +3,8 @@
 set +x
 
 #### BUILD
-if [[ "$TRAVIS_PULL_REQUEST" != "false" ]]; then
+if [[ "$TRAVIS_PULL_REQUEST" != "false" || ( "$TRAVIS_BRANCH" != "master" && "$TRAVIS_BRANCH" != hotfix/* ) ]]; then
     echo "This is a pull request. No deployment will be done."
-    ./gradlew clean testDevDebug -PdisablePreDex
-    exit $?
-elif [[ ( "$TRAVIS_BRANCH" != "master" && "$TRAVIS_BRANCH" != hotfix/* ) ]]; then
-    echo "Testing on a branch other than master or hotfix."
-    export BUILD_NUMBER=$TRAVIS_BUILD_NUMBER
     ./gradlew clean testDevDebug -PdisablePreDex
     exit $?
 else
@@ -17,8 +12,4 @@ else
   export BUILD_NUMBER=$TRAVIS_BUILD_NUMBER
   ./gradlew clean assembleStagingRelease assembleProductionRelease -PdisablePreDex
     exit $?
-else
-  echo "Testing on a branch other than master or release. Running tests"
-  ./gradlew clean testDebug -PdisablePreDex
-  exit $?
 fi
