25f6f429e76c9f73a23e5796b3cf906928acad54
Rashad cureton <rashad.cureton@gmail.com>
2017-03-22 09:43:08 -0400

    made pr changes for the build scripts

--- scripts/build.sh
+++ scripts/build.sh
@@ -4,21 +4,21 @@ set +x
 
 #### BUILD
 if [[ "$TRAVIS_PULL_REQUEST" != "false" ]]; then
-    echo "PR build, running tests"
-    ./gradlew clean test -PdisablePreDex
+    echo "This is a pull request. No deployment will be done."
+    ./gradlew clean testDevDebug -PdisablePreDex
     exit $?
-elif [[ ( "$TRAVIS_BRANCH" == "master" ) ]]; then
-    echo "This is a master merge. Building Release"
+elif [[ ( "$TRAVIS_BRANCH" != "master" && "$TRAVIS_BRANCH" != hotfix/* ) ]]; then
+    echo "Testing on a branch other than master or hotfix."
     export BUILD_NUMBER=$TRAVIS_BUILD_NUMBER
-    ./gradlew clean assembleRelease -PdisablePreDex
-    exit $?
-elif [[ ("$TRAVIS_BRANCH" == release/*) ]]; then
-    echo "Building QA, Staging, and Production releases on $TRAVIS_BRANCH"
-    export BUILD_NUMBER=$TRAVIS_BUILD_NUMBER
-    ./gradlew clean assembleStagingRelease assembleProductionRelease -PdisablePreDex
+    ./gradlew clean testDevDebug -PdisablePreDex
     exit $?
 else
-    echo "Testing on a branch other than master or release. Running tests"
-    ./gradlew clean testDebug -PdisablePreDex
+  echo "Building Staging and Production releases on $TRAVIS_BRANCH"
+  export BUILD_NUMBER=$TRAVIS_BUILD_NUMBER
+  ./gradlew clean assembleStagingRelease assembleProductionRelease -PdisablePreDex
     exit $?
+else
+  echo "Testing on a branch other than master or release. Running tests"
+  ./gradlew clean testDebug -PdisablePreDex
+  exit $?
 fi
