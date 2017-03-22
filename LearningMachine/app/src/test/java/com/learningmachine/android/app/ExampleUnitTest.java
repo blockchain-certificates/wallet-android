4ae5a6a1ad9e53321c40f9db085e008ef0761736
Andrew <alunsford3@gmail.com>
2017-03-22 17:58:18 -0400

    Added BitcoinJ and a test.

--- LearningMachine/app/src/test/java/com/learningmachine/android/app/ExampleUnitTest.java
+++ LearningMachine/app/src/test/java/com/learningmachine/android/app/ExampleUnitTest.java
@@ -1,8 +1,12 @@
 package com.learningmachine.android.app;
 
+import org.bitcoinj.core.Address;
+import org.bitcoinj.core.ECKey;
+import org.bitcoinj.core.NetworkParameters;
+import org.bitcoinj.utils.BriefLogFormatter;
 import org.junit.Test;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 /**
  * Example local unit test, which will execute on the development machine (host).
@@ -10,8 +14,22 @@ import static org.junit.Assert.*;
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
 public class ExampleUnitTest {
+
     @Test
-    public void addition_isCorrect() throws Exception {
-        assertEquals(4, 2 + 2);
+    public void testCertificateVerification() throws Exception {
+        String certSignature = "H0osFKllW8LrBhNMc4gC0TbRU0OK9Qgpebji1PgmNsgtSKCLXHL217cEG3FoHkaF/G2woGaoKDV/MrmpROvD860=";
+        String assertionUid = "609c2989-275f-4f4c-ab02-b245cfb09017";
+
+        ECKey ecKey = ECKey.signedMessageToKey(assertionUid, certSignature);
+        ecKey.verifyMessage(assertionUid, certSignature);
+        NetworkParameters networkParameters = NetworkParameters.fromID(NetworkParameters.ID_MAINNET);
+
+        if (networkParameters == null) {
+            return;
+        }
+
+        Address address = ecKey.toAddress(networkParameters);
+        String issuerKey = "1Q3P94rdNyftFBEKiN1fxmt2HnQgSCB619";
+        assertEquals(issuerKey, address.toBase58());
     }
 }
