84c3f811d6bb171fa2ea26d42f6ba9e1ba9931d4
Andrew <alunsford3@gmail.com>
2017-03-28 17:54:24 -0400

    Added settings icon to home Fragment. [Finishes #142555507]

--- LearningMachine/app/src/main/java/com/learningmachine/android/app/ui/home/HomeFragment.java
+++ LearningMachine/app/src/main/java/com/learningmachine/android/app/ui/home/HomeFragment.java
@@ -8,6 +8,8 @@ import android.support.annotation.Nullable;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
@@ -27,6 +29,12 @@ public class HomeFragment extends LearningMachineFragment {
         return new HomeFragment();
     }
 
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setHasOptionsMenu(true);
+    }
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
@@ -35,6 +43,12 @@ public class HomeFragment extends LearningMachineFragment {
         setupRecyclerView();
 
         return mBinding.getRoot();
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        super.onCreateOptionsMenu(menu, inflater);
+        inflater.inflate(R.menu.fragment_home, menu);
     }
 
     private void setupRecyclerView() {
