b20af5eb7fc3dbe4655dd6f2c8950a8f9f077c75
Andrew <alunsford3@gmail.com>
2017-04-03 11:08:05 -0400

    BTC wip

---
+++ LearningMachine/app/src/main/java/com/learningmachine/android/app/data/bitcoin/BitcoinManager.java
@@ -0,0 +1,24 @@
+package com.learningmachine.android.app.data.bitcoin;
+
+public class BitcoinManager {
+
+/*
+    static func generateSeedPhrase() -> String {
+        let randomData = BTCRandomDataWithLength(32) as Data
+        return generateSeedPhrase(withRandomData: randomData)
+    }
+
+    static func generateSeedPhrase(withRandomData randomData: Data) -> String {
+        let mn = BTCMnemonic(entropy: randomData, password: "", wordListType: .english)
+
+        return mn?.words.flatMap({ $0 as? String }).joined(separator: " ") ?? ""
+    }
+ */
+    public String generateSeedPhrase() {
+//        BTCrandom
+    }
+
+    public String generateSeedPhrase() {
+
+    }
+}
--- LearningMachine/app/src/main/java/com/learningmachine/android/app/data/inject/DataModule.java
+++ LearningMachine/app/src/main/java/com/learningmachine/android/app/data/inject/DataModule.java
@@ -1,8 +1,18 @@
 package com.learningmachine.android.app.data.inject;
 
+import com.learningmachine.android.app.data.bitcoin.BitcoinManager;
+
+import javax.inject.Singleton;
+
 import dagger.Module;
+import dagger.Provides;
 
 @Module
 public class DataModule {
 
+    @Provides
+    @Singleton
+    BitcoinManager providesBitcoinManager() {
+        return new BitcoinManager();
+    }
 }
