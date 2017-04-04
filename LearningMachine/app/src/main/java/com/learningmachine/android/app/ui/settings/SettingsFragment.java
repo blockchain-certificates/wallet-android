9a6bd948c0d597092b04c97a78b37302dd20ef71
Rashad cureton <rashad.cureton@gmail.com>
2017-04-04 17:07:37 -0400

    User can see the privacy policy [Finishes #142558611]

--- LearningMachine/app/src/main/java/com/learningmachine/android/app/ui/settings/SettingsFragment.java
+++ LearningMachine/app/src/main/java/com/learningmachine/android/app/ui/settings/SettingsFragment.java
@@ -1,5 +1,6 @@
 package com.learningmachine.android.app.ui.settings;
 
+import android.content.Intent;
 import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
@@ -8,6 +9,7 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.learningmachine.android.app.R;
+import com.learningmachine.android.app.controller.LMSupportWebActivity;
 import com.learningmachine.android.app.databinding.FragmentSettingsBinding;
 import com.learningmachine.android.app.ui.LMFragment;
 
@@ -24,6 +26,16 @@ public class SettingsFragment extends LMFragment {
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         mBinding = DataBindingUtil.inflate(inflater, R.layout.fragment_settings, container, false);
+
+        mBinding.settingsPrivacyPolicyText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String actionBarTitle = getString(R.string.settings_privacy_policy);
+                String endPoint = getString(R.string.settings_privacy_policy_endpoint);
+                Intent intent = LMSupportWebActivity.newIntent(getContext(), actionBarTitle, endPoint);
+                startActivity(intent);
+            }
+        });
 
         return mBinding.getRoot();
     }
