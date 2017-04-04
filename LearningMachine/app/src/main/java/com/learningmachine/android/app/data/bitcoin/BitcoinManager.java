a6c0d756895af2d2df84a1256a3b3348c2b89dad
Andrew <alunsford3@gmail.com>
2017-04-04 15:00:16 -0400

    Wallet now has a passphrase.

--- LearningMachine/app/src/main/java/com/learningmachine/android/app/data/bitcoin/BitcoinManager.java
+++ LearningMachine/app/src/main/java/com/learningmachine/android/app/data/bitcoin/BitcoinManager.java
@@ -3,52 +3,40 @@ package com.learningmachine.android.app.data.bitcoin;
 import android.os.Environment;
 import android.text.TextUtils;
 
-import com.google.common.util.concurrent.Service;
 import com.learningmachine.android.app.LMConstants;
 import com.learningmachine.android.app.LMNetworkConstants;
 
 import org.bitcoinj.kits.WalletAppKit;
 import org.bitcoinj.wallet.DeterministicSeed;
 import org.bitcoinj.wallet.Wallet;
-import org.bitcoinj.wallet.listeners.WalletChangeEventListener;
 
 import java.io.File;
 import java.util.List;
 
+import timber.log.Timber;
+
 public class BitcoinManager {
 
-    /*
-        static func generateSeedPhrase() -> String {
-            let randomData = BTCRandomDataWithLength(32) as Data
-            return generateSeedPhrase(withRandomData: randomData)
-        }
-
-        static func generateSeedPhrase(withRandomData randomData: Data) -> String {
-            let mn = BTCMnemonic(entropy: randomData, password: "", wordListType: .english)
-
-            return mn?.words.flatMap({ $0 as? String }).joined(separator: " ") ?? ""
-        }
-     */
     private WalletAppKit mWalletAppKit;
     private Wallet mWallet;
 
     public BitcoinManager() {
         File path = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOCUMENTS + "/bitcoin/");
-        WalletAppKit kit = new WalletAppKit(LMNetworkConstants.getNetwork(), path, LMConstants.WALLET_FILE);
+        WalletAppKit kit = new WalletAppKit(LMNetworkConstants.getNetwork(), path, LMConstants.WALLET_FILE) {
+            @Override
+            protected void onSetupCompleted() {
+                super.onSetupCompleted();
+                mWallet = wallet();
+                DeterministicSeed seed = mWallet.getKeyChainSeed();
+                List<String> mnemonicCode = seed.getMnemonicCode();
+                String passphrase = TextUtils.join(" ", mnemonicCode);
+                Timber.d("Passphrase: %1$s", passphrase);
+            }
+        };
         kit.setAutoSave(true);
         kit.startAsync();
-//        mWallet.addChangeEventListener(new WalletChangeEventListener() {
-//            @Override
-//            public void onWalletChanged(Wallet wallet) {
-//                wallet.get
-//            }
-//        });
-//        kit.startAndWait();
-        //        kit.awaitRunning();
-        //      kit.wallet().reset();
-//        peerGroup = kit.peerGroup();
 
-        mWallet = kit.wallet();
+
     }
 
     public String getPassphrase() {
