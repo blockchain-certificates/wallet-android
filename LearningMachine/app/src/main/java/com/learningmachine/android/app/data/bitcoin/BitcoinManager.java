ea23e801a43b24262dcc201d9868f7ee2c6571cd
Andrew <alunsford3@gmail.com>
2017-04-05 13:02:38 -0400

    Added StringUtils to replace TextUtils in tests. Cleaned up BitcoinManager

--- LearningMachine/app/src/main/java/com/learningmachine/android/app/data/bitcoin/BitcoinManager.java
+++ LearningMachine/app/src/main/java/com/learningmachine/android/app/data/bitcoin/BitcoinManager.java
@@ -1,21 +1,17 @@
 package com.learningmachine.android.app.data.bitcoin;
 
 import android.content.Context;
-import android.os.Environment;
-import android.text.TextUtils;
 
-import com.learningmachine.android.app.LMConstants;
 import com.learningmachine.android.app.LMNetworkConstants;
+import com.learningmachine.android.app.util.StringUtils;
 
 import org.bitcoinj.core.NetworkParameters;
 import org.bitcoinj.crypto.MnemonicCode;
 import org.bitcoinj.crypto.MnemonicException;
-import org.bitcoinj.kits.WalletAppKit;
 import org.bitcoinj.wallet.DeterministicSeed;
 import org.bitcoinj.wallet.KeyChainGroup;
 import org.bitcoinj.wallet.Wallet;
 
-import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.security.SecureRandom;
@@ -30,57 +26,38 @@ public class BitcoinManager {
 
     public BitcoinManager(Context context) {
         mContext = context;
-//        setupWalletAppKit();
-        setupWallet();
+        createWallet();
     }
 
-    private void setupWalletAppKit() {
-        File path = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOCUMENTS + "/bitcoin/");
-        WalletAppKit kit = new WalletAppKit(LMNetworkConstants.getNetwork(), path, LMConstants.WALLET_FILE) {
-            @Override
-            protected void onSetupCompleted() {
-                super.onSetupCompleted();
-                mWallet = wallet();
-                DeterministicSeed seed = mWallet.getKeyChainSeed();
-                List<String> mnemonicCode = seed.getMnemonicCode();
-                String passphrase = TextUtils.join(" ", mnemonicCode);
-                Timber.d("Passphrase: %1$s", passphrase);
-            }
-        };
-        kit.setAutoSave(true);
-        kit.startAsync();
-    }
-
-    private void setupWallet() {
+    private void createWallet() {
         SecureRandom random = new SecureRandom();
         byte[] seedData = random.generateSeed(32);
-        List<String> passphrase= generateMnemonicCode(seedData);
-        String passphraseString = TextUtils.join(" ", passphrase);
-        Timber.d("Before wallet: %1$s", passphraseString);
-
-        DeterministicSeed deterministicSeed = new DeterministicSeed(passphrase, seedData, "", 0);
+        List<String> mnemonic = generateMnemonic(seedData);
+        if (mnemonic == null) {
+            Timber.e("Mnemonic is null, wallet creation failure");
+            return;
+        }
+        DeterministicSeed deterministicSeed = new DeterministicSeed(mnemonic, seedData, "", 0);
         NetworkParameters networkParameters = LMNetworkConstants.getNetwork();
         KeyChainGroup keyChainGroup = new KeyChainGroup(networkParameters, deterministicSeed);
 
-        Wallet wallet = new Wallet(networkParameters, keyChainGroup);
-
-        passphraseString = TextUtils.join(" ", wallet.getKeyChainSeed().getMnemonicCode());
-        Timber.d("After wallet: %1$s", passphraseString);
+        mWallet = new Wallet(networkParameters, keyChainGroup);
+        // write wallet to file
     }
 
-    private List<String> generateMnemonicCode(byte[] seedData) {
+    private List<String> generateMnemonic(byte[] seedData) {
         try {
             InputStream inputStream = mContext.getAssets()
                     .open("english.txt");
             MnemonicCode mnemonicCode = new MnemonicCode(inputStream, null);
-            List<String> words = mnemonicCode.toMnemonic(seedData);
-            return words;
+            return mnemonicCode.toMnemonic(seedData);
         } catch (IOException e) {
+            Timber.e(e, "Unable to read word list.");
             e.printStackTrace();
         } catch (MnemonicException.MnemonicLengthException e) {
+            Timber.e(e, "Unable to create mnemonic from word list");
             e.printStackTrace();
         }
-
         return null;
     }
 
@@ -90,6 +67,6 @@ public class BitcoinManager {
         }
         DeterministicSeed seed = mWallet.getKeyChainSeed();
         List<String> mnemonicCode = seed.getMnemonicCode();
-        return TextUtils.join(" ", mnemonicCode);
+        return StringUtils.join(" ", mnemonicCode);
     }
 }
