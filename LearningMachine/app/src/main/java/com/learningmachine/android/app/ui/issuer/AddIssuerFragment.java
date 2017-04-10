0db9bff4fd474ef7fee5cc1b3da2d616c6268aea
Rashad cureton <rashad.cureton@gmail.com>
2017-04-10 15:22:27 -0400

    [Finishes #143204949] added toolbar for the addIssuer fragment

--- LearningMachine/app/src/main/java/com/learningmachine/android/app/ui/issuer/AddIssuerFragment.java
+++ LearningMachine/app/src/main/java/com/learningmachine/android/app/ui/issuer/AddIssuerFragment.java
@@ -4,6 +4,9 @@ import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 
@@ -25,5 +28,27 @@ public class AddIssuerFragment extends LMFragment {
         mBinding = DataBindingUtil.inflate(inflater, R.layout.fragment_add_issuer, container, false);
 
         return mBinding.getRoot();
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setHasOptionsMenu(true);
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        inflater.inflate(R.menu.fragment_add_issuer, menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch (item.getItemId()) {
+           case  R.id.fragment_add_issuer_verify:
+
+               break;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
