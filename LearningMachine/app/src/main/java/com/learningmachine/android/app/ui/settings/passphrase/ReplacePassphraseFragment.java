756b5c524cfe7de02b0be78c3e5a63b231fb8cfe
Andrew <alunsford3@gmail.com>
2017-04-12 11:55:46 -0400

    Added done icon to ReplacePassphraseFragment [Finishes #142558417]

--- LearningMachine/app/src/main/java/com/learningmachine/android/app/ui/settings/passphrase/ReplacePassphraseFragment.java
+++ LearningMachine/app/src/main/java/com/learningmachine/android/app/ui/settings/passphrase/ReplacePassphraseFragment.java
@@ -5,6 +5,9 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 
@@ -18,6 +21,12 @@ public class ReplacePassphraseFragment extends LMFragment {
         return new ReplacePassphraseFragment();
     }
 
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setHasOptionsMenu(true);
+    }
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
@@ -26,5 +35,21 @@ public class ReplacePassphraseFragment extends LMFragment {
                 container,
                 false);
         return binding.getRoot();
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        super.onCreateOptionsMenu(menu, inflater);
+        inflater.inflate(R.menu.fragment_replace_passphrase, menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch (item.getItemId()) {
+            case R.id.fragment_replace_passphrase_done_menu_item:
+                break;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
