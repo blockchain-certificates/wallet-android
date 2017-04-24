3ae6e2c8e740f33db05ebd5af3108294ab3f8264
Andrew <alunsford3@gmail.com>
2017-04-24 18:19:55 -0400

    Load wallet kinda figured out maybe?

--- LearningMachine/app/src/main/java/com/learningmachine/android/app/data/bitcoin/BitcoinManager.java
+++ LearningMachine/app/src/main/java/com/learningmachine/android/app/data/bitcoin/BitcoinManager.java
@@ -7,11 +7,17 @@ import com.learningmachine.android.app.util.ListUtils;
 import com.learningmachine.android.app.util.StringUtils;
 
 import org.bitcoinj.core.NetworkParameters;
+import org.bitcoinj.kits.WalletAppKit;
 import org.bitcoinj.wallet.DeterministicSeed;
 import org.bitcoinj.wallet.KeyChainGroup;
+import org.bitcoinj.wallet.Protos;
 import org.bitcoinj.wallet.Wallet;
+import org.bitcoinj.wallet.WalletExtension;
+import org.bitcoinj.wallet.WalletProtobufSerializer;
 
+import java.io.FileInputStream;
 import java.security.SecureRandom;
+import java.util.ArrayList;
 import java.util.List;
 
 import timber.log.Timber;
@@ -42,6 +48,26 @@ public class BitcoinManager {
 
         mWallet = new Wallet(networkParameters, keyChainGroup);
         // write wallet to file
+    }
+
+    private void DELETE_ME() {
+//        WalletAppKit walletAppKit = new WalletAppKit();
+    }
+
+    private Wallet loadWallet() throws Exception {
+        // check wallet exists
+        Wallet wallet;
+        FileInputStream walletStream = new FileInputStream("whatever.wallet");
+        try {
+            WalletExtension[] extensions = {};
+            Protos.Wallet proto = WalletProtobufSerializer.parseToProto(walletStream);
+            WalletProtobufSerializer serializer = new WalletProtobufSerializer();
+            NetworkParameters networkParameters = LMNetworkConstants.getNetwork();
+            wallet = serializer.readWallet(networkParameters, extensions, proto);
+        } finally {
+            walletStream.close();
+        }
+        return wallet;
     }
 
     public String getPassphrase() {
