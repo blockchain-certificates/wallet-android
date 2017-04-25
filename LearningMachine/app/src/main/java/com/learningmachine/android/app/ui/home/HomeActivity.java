f21d6d68c114f3f2e67da56ff7008e08b0683436
Rashad cureton <rashad.cureton@gmail.com>
2017-04-25 12:43:06 -0400

    added override method to HomeActivity to set HomeFragment title

--- LearningMachine/app/src/main/java/com/learningmachine/android/app/ui/home/HomeActivity.java
+++ LearningMachine/app/src/main/java/com/learningmachine/android/app/ui/home/HomeActivity.java
@@ -1,7 +1,10 @@
 package com.learningmachine.android.app.ui.home;
 
+import android.app.ActionBar;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 
+import com.learningmachine.android.app.R;
 import com.learningmachine.android.app.ui.LMSingleFragmentActivity;
 
 public class HomeActivity extends LMSingleFragmentActivity {
@@ -9,5 +12,16 @@ public class HomeActivity extends LMSingleFragmentActivity {
     @Override
     protected Fragment createFragment() {
         return HomeFragment.newInstance();
+    }
+
+    @Nullable
+    @Override
+    public ActionBar getActionBar() {
+        return super.getActionBar();
+    }
+
+    @Override
+    public String getActionBarTitle() {
+        return getString(R.string.home_issuers);
     }
 }
--- LearningMachine/app/src/main/java/com/learningmachine/android/app/ui/home/HomeFragment.java
+++ LearningMachine/app/src/main/java/com/learningmachine/android/app/ui/home/HomeFragment.java
@@ -5,7 +5,6 @@ import android.content.Intent;
 import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
-import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
@@ -44,7 +43,6 @@ public class HomeFragment extends LMFragment {
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         mBinding = DataBindingUtil.inflate(inflater, R.layout.fragment_home, container, false);
-        ((AppCompatActivity)getActivity()).getSupportActionBar().setTitle(R.string.home_issuers);
 
         setupRecyclerView();
 
