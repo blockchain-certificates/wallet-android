4bfb3e6c8fbe7d437f507af81cf112e0a1411ec0
Bolot Kerimbaev <bolot@bignerdranch.com>
2017-05-01 15:55:13 -0400

    Build staging and prod

--- scripts/build.sh
+++ scripts/build.sh
@@ -3,6 +3,16 @@
 set +x
 cd LearningMachine
 
+# The below block is temporary while we're troubleshooting CI builds
+echo "Bolot: CI troubleshooting"
+echo "Building Staging and Production releases on $TRAVIS_BRANCH"
+export BUILD_NUMBER=42
+./gradlew clean
+./gradlew assembleStagingRelease  -PdisablePreDex
+./gradlew assembleProductionRelease -PdisablePreDex
+exit $?
+
+
 #### BUILD
 if [[ "$TRAVIS_PULL_REQUEST" != "false" || ( "$TRAVIS_BRANCH" != "master" && "$TRAVIS_BRANCH" != hotfix/* ) ]]; then
     echo "This is a pull request. No deployment will be done."
