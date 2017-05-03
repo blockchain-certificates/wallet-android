ab3ec7e82230487f640f73c0f880f0516a57c0ce
Andrew <alunsford3@gmail.com>
2017-05-03 17:29:18 -0400

    Added sample cert file. Added JS to point to the local json file to load a cert. Added method to CertStore to return the local filepath of a cert. [Finishes #144725093]

--- LearningMachine/app/src/main/java/com/learningmachine/android/app/ui/cert/CertificateFragment.java
+++ LearningMachine/app/src/main/java/com/learningmachine/android/app/ui/cert/CertificateFragment.java
@@ -16,13 +16,22 @@ import android.webkit.WebView;
 import android.webkit.WebViewClient;
 
 import com.learningmachine.android.app.R;
+import com.learningmachine.android.app.data.inject.Injector;
 import com.learningmachine.android.app.data.model.Certificate;
+import com.learningmachine.android.app.data.store.CertificateStore;
 import com.learningmachine.android.app.databinding.FragmentCertificateBinding;
 import com.learningmachine.android.app.ui.LMFragment;
+
+import javax.inject.Inject;
 
 public class CertificateFragment extends LMFragment {
 
     private static final String ARG_CERTIFICATE = "CertificateFragment.Certificate";
+    private static final String INDEX_FILE_PATH = "file:///android_asset/www/index.html";
+    private static final String JS_OPEN = "javascript:(function() { document.getElementsByTagName('blockchain-certificate')[0].href='";
+    private static final String JS_CLOSE = "';})()";
+
+    @Inject protected CertificateStore mCertificateStore;
 
     private Certificate mCertificate;
     private FragmentCertificateBinding mBinding;
@@ -41,6 +50,8 @@ public class CertificateFragment extends LMFragment {
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setHasOptionsMenu(true);
+        Injector.obtain(getContext())
+                .inject(this);
 
         mCertificate = (Certificate) getArguments().getSerializable(ARG_CERTIFICATE);
     }
@@ -83,13 +94,13 @@ public class CertificateFragment extends LMFragment {
         // Ensure local links/redirects in WebView, not the browser.
         mBinding.webView.setWebViewClient(new LMWebViewClient());
 
-        mBinding.webView.loadUrl("file:///android_asset/www/index.html");
+        mBinding.webView.loadUrl(INDEX_FILE_PATH);
     }
 
     public class LMWebViewClient extends WebViewClient {
         @Override
         public boolean shouldOverrideUrlLoading(WebView view, String url) {
-            // Handle local URLs.
+            // Handle local URLs
             if (Uri.parse(url)
                     .getHost()
                     .length() == 0) {
@@ -97,9 +108,16 @@ public class CertificateFragment extends LMFragment {
             }
 
             Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
-            view.getContext()
-                    .startActivity(intent);
+            startActivity(intent);
             return true;
+        }
+
+        @Override
+        public void onPageFinished(WebView view, String url) {
+            // uuid is currently wrong, but will be fixed when certs are actually added & saved
+            String certFilePath = mCertificateStore.getCertificateJsonFile(mCertificate.getUuid());
+            String javascript =  JS_OPEN + certFilePath + JS_CLOSE;
+            mBinding.webView.loadUrl(javascript);
         }
     }
 }
