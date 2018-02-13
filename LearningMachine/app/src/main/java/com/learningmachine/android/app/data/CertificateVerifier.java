ce1cc227895bdf5d8f4331b93fed5f1032ef634e
Rocco Bowling <rocco@chimerasw.com>
2018-02-13 16:00:41 -0500

    refixed identification of testnet cert and early bail out of verification

--- LearningMachine/app/src/main/java/com/learningmachine/android/app/data/CertificateVerifier.java
+++ LearningMachine/app/src/main/java/com/learningmachine/android/app/data/CertificateVerifier.java
@@ -73,6 +73,22 @@ public class CertificateVerifier {
         try (FileInputStream inputStream = new FileInputStream(file)) {
             BlockCertParser blockCertParser = new BlockCertParser();
             BlockCert blockCert = blockCertParser.fromJson(inputStream);
+
+
+            String receiverKey = blockCert.getRecipientPublicKey();
+            String issuerKey = blockCert.getVerificationPublicKey();
+
+            // TODO: REMOVE THIS AND ALLOW ACTUAL VERIFICATIONS OF CROSS CHAIN CERTIFICATES
+            // testnet check: if either the receiver or the issuer originated
+            // from testnet then we will not validate using the normal method
+            if(receiverKey != null && issuerKey != null) {
+                if(receiverKey.startsWith("m") || receiverKey.startsWith("n") || issuerKey.startsWith("m") || issuerKey.startsWith("n")) {
+                    Timber.e("ComparingExpectedMerkleRootWithValueOnTheBlockchain - failed");
+                    return Observable.error(new ExceptionWithResourceString(R.string.error_testnet_certificate_json));
+                }
+            }
+
+
             return Observable.just(blockCert);
         } catch (IOException | NoSuchElementException e) {
             Timber.e(e, "Could not read certificate file");
@@ -88,6 +104,7 @@ public class CertificateVerifier {
     }
 
     public Observable<Object> CompareComputedHashWithExpectedHash(BlockCert certificate, TxRecord txRecord) {
+
         String possibleHash = certificate.getReceiptHash();
         if (possibleHash == null) {
             possibleHash = txRecord.getRemoteHash();
@@ -143,19 +160,6 @@ public class CertificateVerifier {
     }
 
     public Observable<Object> ComparingExpectedMerkleRootWithValueOnTheBlockchain(BlockCert certificate, TxRecord txRecord) {
-        String receiverKey = certificate.getRecipientPublicKey();
-        String issuerKey = certificate.getVerificationPublicKey();
-
-        // TODO: REMOVE THIS AND ALLOW ACTUAL VERIFICATIONS OF CROSS CHAIN CERTIFICATES
-        // testnet check: if either the receiver or the issuer originated
-        // from testnet then we will not validate using the normal method
-        if(receiverKey != null && issuerKey != null) {
-            if(receiverKey.startsWith("m") || receiverKey.startsWith("n") || issuerKey.startsWith("m") || issuerKey.startsWith("n")) {
-                Timber.e("ComparingExpectedMerkleRootWithValueOnTheBlockchain - failed");
-                return Observable.error(new ExceptionWithResourceString(R.string.error_testnet_certificate_json));
-            }
-        }
-
         return Observable.create(emitter -> {
             String merkleRoot = certificate.getMerkleRoot();
             String remoteHash = txRecord.getRemoteHash();
