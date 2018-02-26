5f7b0838bcc9bf942500dd11940bfeb71520e9c0
Rocco Bowling <rocco@chimerasw.com>
2018-02-26 16:09:49 -0500

    WIP: redesign of new account view to accomodate video

--- LearningMachine/app/src/main/java/com/learningmachine/android/app/ui/onboarding/AccountChooserFragment.java
+++ LearningMachine/app/src/main/java/com/learningmachine/android/app/ui/onboarding/AccountChooserFragment.java
@@ -2,16 +2,25 @@ package com.learningmachine.android.app.ui.onboarding;
 
 import android.app.Fragment;
 import android.content.Context;
+import android.content.Intent;
 import android.databinding.DataBindingUtil;
+import android.graphics.Point;
+import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Display;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.FrameLayout;
+import android.widget.TextView;
+import android.widget.VideoView;
 
 import com.learningmachine.android.app.R;
 import com.learningmachine.android.app.data.preferences.SharedPreferencesManager;
 import com.learningmachine.android.app.databinding.FragmentAccountChooserBinding;
+import com.learningmachine.android.app.dialog.AlertDialogFragment;
+import com.learningmachine.android.app.util.DialogUtils;
 import com.smallplanet.labalib.Laba;
 
 import javax.inject.Inject;
@@ -40,10 +49,12 @@ public class AccountChooserFragment extends OnboardingFragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         mBinding = DataBindingUtil.inflate(inflater, R.layout.fragment_account_chooser, container, false);
 
-
         Laba.Animate(mBinding.newAccountButton, "!^300", () -> { return null; });
         Laba.Animate(mBinding.existingAccountButton, "!^300", () -> { return null; });
 
+        mBinding.playVideo.setOnClickListener(view2 -> {
+
+        });
 
         mBinding.newAccountButton.setOnClickListener(view -> mCallback.onNewAccount());
         mBinding.existingAccountButton.setOnClickListener(view -> mCallback.onExistingAccount());
