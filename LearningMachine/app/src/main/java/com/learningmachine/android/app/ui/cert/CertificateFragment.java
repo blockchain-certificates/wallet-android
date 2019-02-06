679ae54b94add65cfd06a80e20b738f3e0bea76b
Anthony Ronning <anthonyronning@gmail.com>
2019-02-05 18:14:51 -0600

    Replaces hashtags with appropriate encoding characters for google 72
    Google 72 broke the app from loading in certificates that have hashtags in them. https://www.chromestatus.com/features/5656049583390720

--- LearningMachine/app/src/main/java/com/learningmachine/android/app/ui/cert/CertificateFragment.java
+++ LearningMachine/app/src/main/java/com/learningmachine/android/app/ui/cert/CertificateFragment.java
@@ -159,7 +159,8 @@ public class CertificateFragment extends LMFragment {
         String customCss = "body {padding: 20px; font-size: 12px; line-height: 1.5;} body > section { padding: 0;} body section { max-width: 100%; } body img { max-width: 100%; height: auto; width: inherit; }";
         String wrappedHtml = String.format("<!doctype html><html class=\"no-js\" lang=\"\"><head><meta charset=\"utf-8\"><meta http-equiv=\"x-ua-compatible\" content=\"ie=edge\"><title></title><meta content=\"width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0\" name=\"viewport\" /><meta name=”viewport” content=”width=device-width” /><style type=\"text/css\">%s</style><style type=\"text/css\">%s</style></head><body>%s</body></html>", normalizeCss, customCss, displayHTML);
 
-        return wrappedHtml;
+        // google 72 does not allow '#' and requires replacing with %23
+        return wrappedHtml.replaceAll("#","%23");
     }
 
     private void setupWebView() {
