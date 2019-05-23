42d4259cb36e29921b71eb610018f850f83bb303
Trey Valenta <t@trey.net>
2019-05-23 13:11:47 -0700

    cat the error file if it exists

---
+++ LearningMachine/cat-results.sh
@@ -0,0 +1,3 @@
+#! /bin/bash
+
+[ -e /home/travis/build/blockchain-certificates/wallet-android/LearningMachine/app/build/reports/tests/testDevDebugUnitTest/index.html ] && cat /home/travis/build/blockchain-xcertificates/wallet-android/LearningMachine/app/build/reports/tests/testDevDebugUnitTest/index.html
