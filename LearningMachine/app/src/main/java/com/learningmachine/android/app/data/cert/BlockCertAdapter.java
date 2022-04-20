25b078313f12662c53394229b8134127a081c371
Julien Fraichot <julien.fraichot@hyland.com>
2022-04-20 12:04:05 -0500

    refactor(BlockCertAdapter): rely on @context property to figure out blockcerts version

--- LearningMachine/app/src/main/java/com/learningmachine/android/app/data/cert/BlockCertAdapter.java
+++ LearningMachine/app/src/main/java/com/learningmachine/android/app/data/cert/BlockCertAdapter.java
@@ -3,6 +3,7 @@ package com.learningmachine.android.app.data.cert;
 import com.google.gson.JsonDeserializationContext;
 import com.google.gson.JsonDeserializer;
 import com.google.gson.JsonElement;
+import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParseException;
 import com.google.gson.JsonSerializationContext;
@@ -12,6 +13,8 @@ import com.learningmachine.android.app.data.cert.v12.BlockCertV12;
 import com.learningmachine.android.app.data.cert.v20.BlockCertV20;
 
 import java.lang.reflect.Type;
+import java.util.ArrayList;
+import java.util.regex.*;
 
 public class BlockCertAdapter implements JsonSerializer<BlockCert>, JsonDeserializer<BlockCert> {
     @Override
@@ -36,14 +39,72 @@ public class BlockCertAdapter implements JsonSerializer<BlockCert>, JsonDeserial
     }
 
     private Class<?> getAdapterClass (JsonObject jsonObject) {
-        if (isV20(jsonObject)) {
-            return BlockCertV20.class;
-        } else if (isV12(jsonObject)) {
-            return BlockCertV12.class;
-        } else if (isV11(jsonObject)) {
-            return BlockCertV11.class;
+        final String version = getBlockcertsVersion(jsonObject);
+        switch (version) {
+            case "v1.1":
+                return BlockCertV11.class;
+
+            case "v1":
+            case "v1.2":
+                return BlockCertV12.class;
+
+            case "v2":
+            case "v2.0":
+            case "v2.1":
+                return BlockCertV20.class;
+
+            default:
+                return null;
         }
-        return null;
+    }
+
+    private String getBlockcertsVersion (JsonObject jsonObject) {
+        if (jsonObject.get("@context") == null && isV11(jsonObject)) {
+            return "v1.1";
+        }
+        ArrayList contextArray = new ArrayList<String>();
+        final JsonElement context = jsonObject.get("@context");
+        if (context.isJsonArray()) {
+            final JsonArray contextObjectAsArray = jsonObject.getAsJsonArray("@context");
+            for (int i = 0; i < contextObjectAsArray.size(); i++) {
+                if (!contextObjectAsArray.get(i).isJsonObject()) {
+                    contextArray.add(contextObjectAsArray.get(i).toString());
+                }
+            }
+        } else {
+            contextArray.add(context.toString());
+        }
+
+        System.out.println(contextArray);
+
+        final String blockcertsContextUrl = filterBlockcertsContext(contextArray);
+        System.out.println(blockcertsContextUrl);
+        if (blockcertsContextUrl != "") {
+            final String version = getVersionNumber(blockcertsContextUrl);
+            System.out.println(version);
+            if (version.startsWith("v")) {
+                return version;
+            }
+            return "v" + version;
+        }
+        return "invalid blockcerts version";
+    }
+
+    private String filterBlockcertsContext(ArrayList<String> contextArray) {
+        Object[] result = contextArray.stream().filter(s -> s.contains("blockcerts")).toArray();
+        if (result.length != 0) {
+            return result[0].toString();
+        }
+        return "";
+    }
+
+    private String getVersionNumber (String blockcertsContextUrl) {
+        Pattern versionRegexPattern = Pattern.compile("/(v?\\d{1}.?\\d?\\b)");
+        Matcher matcher = versionRegexPattern.matcher(blockcertsContextUrl);
+        if (matcher.find()) {
+            return matcher.group(1);
+        }
+        return "";
     }
 
     private boolean isV11(JsonObject jsonObject) {
@@ -53,26 +114,6 @@ public class BlockCertAdapter implements JsonSerializer<BlockCert>, JsonDeserial
                 && jsonObject.get("recipient") != null
                 && jsonObject.get("signature") != null
                 && jsonObject.get("@context") == null) {
-            return true;
-        }
-        return false;
-    }
-
-    private boolean isV12(JsonObject jsonObject) {
-        if (jsonObject.get("@context") != null
-                && jsonObject.get("type") != null
-                && jsonObject.get("document") != null
-                && jsonObject.get("receipt") != null) {
-            return true;
-        }
-        return false;
-    }
-
-    private boolean isV20(JsonObject jsonObject) {
-        if (jsonObject.get("type") != null
-                && jsonObject.get("badge") != null
-                && jsonObject.get("signature") != null
-                && jsonObject.get("recipient") != null) {
             return true;
         }
         return false;
